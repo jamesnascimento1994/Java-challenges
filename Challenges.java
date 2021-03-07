51164e4811984d49516b4d522437f71a154eaf76
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-07 09:52:14 -0500

    solution for buggy code

---
+++ Challenges.java
@@ -0,0 +1,32 @@
+/*
+ * Challenge One: This code does not execute properly. Try to figure out why.
+ * Challenge Source: https://www.codewars.com/kata/50654ddff44f800200000004/java
+ */
+
+/* Starter code */
+/*
+public class Challenges {
+    public static Double multiply(Double a, Double b) {
+        return a * b
+    }
+}
+*/
+/* 
+NOTE: The code on my repository is going to look slightly different than on codewars. I used integers instead of doubles because it's easier to run tests on VS Code for whatever reason. 
+*/
+/* Solution */
+/*
+public class Challenges {
+    public static Integer multiply(Integer a, Integer b) {
+        return a * b;
+    }
+    */
+    /* TESTS */
+    /*
+    public static void main(String[] args) {
+        System.out.println(multiply(7, 8));
+        System.out.println(multiply(5, 4));
+        System.out.println(multiply(6, 2));
+    }
+}
+*/
