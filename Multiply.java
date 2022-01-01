db9d5daf80d06436a048e98fadf310f9a2a08661
jamesnascimento1994 <james.nascimento94@yahoo.com>
2022-01-01 17:28:08 -0500

    added TenMinWalk function

--- Multiply.java
+++ Multiply.java
@@ -5,7 +5,7 @@
 
 /* Starter code */
 /*
-public class Challenges {
+public class Multiply {
     public static Double multiply(Double a, Double b) {
         return a * b
     }
@@ -15,18 +15,18 @@ public class Challenges {
 NOTE: The code on my repository is going to look slightly different than on codewars. I used integers instead of doubles because it's easier to run tests on VS Code for whatever reason. 
 */
 /* Solution */
-/*
-public class Challenges {
+
+public class Multiply {
     public static Integer multiply(Integer a, Integer b) {
         return a * b;
     }
-    */
+    
     /* TESTS */
-    /*
+    
     public static void main(String[] args) {
         System.out.println(multiply(7, 8));
         System.out.println(multiply(5, 4));
         System.out.println(multiply(6, 2));
     }
 }
-*/
+
